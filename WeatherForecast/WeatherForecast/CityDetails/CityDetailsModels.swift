008628425bc5eeae74c41c16f961139127cdcced
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2022-02-24 15:26:00 +0300

    Create City detail models

---
+++ WeatherForecast/WeatherForecast/CityDetails/CityDetailsModels.swift
@@ -0,0 +1,66 @@
+//
+//  CityDetailsModels.swift
+//  WeatherForecast
+//
+//  Created by MacOS on 24.02.2022.
+//
+
+import UIKit
+
+enum CityDetails
+{
+    enum Fetch
+    {
+        struct Request
+        {
+            
+        }
+        
+        struct Response
+        {
+            let city: CityDetailsResponse?
+            
+        }
+        
+        struct ViewModel
+        {
+            let time : String?
+            let sun_rise : String?
+            let sun_set : String?
+            let title : String?
+        }
+    }
+}
+
+enum Weather
+{
+    enum Fetch
+    {
+        
+        struct Response
+        {
+            let weatherDetails: [WeatherDetails]
+            
+        }
+        
+        struct ViewModel
+        {
+            var consolidatedWeather: [Weather.Fetch.ViewModel.Consolidated]
+            
+            struct Consolidated {
+                
+            let weather_state_name : String?
+            let applicable_date : String?
+            let min_temp : Double?
+            let max_temp : Double?
+            let the_temp : Double?
+            let wind_speed : Double?
+            let wind_direction : Double?
+            let air_pressure : Double?
+            let humidity : Int?
+                
+            }
+        }
+    }
+}
+
