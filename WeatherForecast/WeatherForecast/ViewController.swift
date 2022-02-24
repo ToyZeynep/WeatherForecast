7353dcc6e5073b3ab62d1f1fd28d501adaa6ff04
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2022-02-24 10:48:29 +0300

    Create Project

---
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherForecast
+//
+//  Created by MacOS on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
