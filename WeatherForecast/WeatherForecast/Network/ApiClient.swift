673e2f4ca6f5aeb06fb425b2f83782903b68439d
SaniyeToy <75203610+SaniyeToy@users.noreply.github.com>
2022-02-28 19:28:38 +0300

    Edit code

--- WeatherForecast/WeatherForecast/Network/ApiClient.swift
+++ WeatherForecast/WeatherForecast/Network/ApiClient.swift
@@ -11,9 +11,7 @@ import Alamofire
 class ApiClient {
 
     static func request<T: Decodable>(_ urlConvertible: URLRequestConvertible, completion: @escaping (Result<T, Error>) -> Void) {
-        print(urlConvertible.urlRequest)
        AF.request(urlConvertible).responseDecodable { (response: AFDataResponse<T>) in
-           print(response.response)
             switch response.result {
             case .success(let responseData):
                 completion(.success(responseData))
